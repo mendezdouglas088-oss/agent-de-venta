f0728297d9aa2e74984db4ef9f683fcfdfa4980d
mendezdouglas088-oss <Mendezdouglas088@gmail.com>
2026-08-26 21:02:02 -0400

    feat(backend): agregar Swagger para documentación de la API y actualizar dependencias

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '20mb' }));
   app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Ejemplo')
+    .setDescription('Documentación de la API')
+    .setVersion('1.0')
+    .addTag('users') // Etiqueta para agrupar los endpoints
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
